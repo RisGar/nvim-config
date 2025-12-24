3c6f008145a642588aafb5edaaff624a24f6d7d2
Rishab Garg <me@rishab-garg.me>
2025-12-24 19:14:47 +0100

    fix: always include nixCats in lazydev

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -1,10 +1,6 @@
-vim.cmd.packadd("lazydev.nvim")
 require("lazydev").setup({
 	library = {
-		-- nixCats
-		{ path = nixCats.nixCatsPath and nixCats.nixCatsPath .. "lua" or nil, words = { "nixCats" } },
-		-- See the configuration section for more details
-		-- Load luvit types when the `vim.uv` word is found
+		{ path = nixCats.nixCatsPath .. "lua", words = { "nixCats" } },
 		{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
 	},
 })
