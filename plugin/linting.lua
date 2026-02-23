3d1ef9b16e6eb2ebb87ea7fd17dbcb098c6171ca
Rishab Garg <mail@rishab-garg.de>
2026-02-24 00:54:14 +0100

    feat: udpated linting preferences

--- plugin/linting.lua
+++ plugin/linting.lua
@@ -5,7 +5,7 @@ end
 require("lint").linters_by_ft = {
 	fish = { "fish" },
 	markdown = { "markdownlint-cli2" },
-	python = { "ruff" },
+	nix = { "statix" },
 }
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
